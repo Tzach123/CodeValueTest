2e7427870d9f35005620ac1bf5d8c8d8b3e028aa
Devskiller <owl@devskiller.com>
2021-10-27 10:28:21 +0000

    Candidate repo initialization

---
+++ src/crawler.js
@@ -0,0 +1,10 @@
+const scrapeLinks = require('./utils/scraper');
+
+module.exports = function crawlUrls(paths, depth, http) {
+  /*
+    As an example, the following extracts links from given website URL or file path
+    scrapeLinks('htmls/1.html', false).then(console.log);
+    scrapeLinks('https://edition.cnn.com/', true).then(console.log);
+  */
+  return [];
+}
